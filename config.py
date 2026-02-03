738eee5e8693e558e7b1178bf41b6de5eae2a385
Arpita Mishra <arpitamishra2755@gmail.com>
2026-02-03 20:18:14 +0530

    Updated config file for deplyoment

--- config.py
+++ config.py
@@ -4,6 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-fallback-key")
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///local.db"
+    )
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
